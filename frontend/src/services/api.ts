aeec76fda3333462c3061878257e02d4e798646b
JuscelinoMamede <juscemamede01@gmail.com>
2024-03-07 11:24:26 -0300

    ajuste para vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,3 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "https://crud-mern-six.vercel.app",
-});
+export const api = axios.create({ baseURL: "http://localhost:3333" });
